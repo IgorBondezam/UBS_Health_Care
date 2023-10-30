8804824bb2aa157c2fb4d5b1d606fa4d2b13ade0
Igor Bondezam França <igorbfraca@hotmail.com>
2023-10-29 21:39:18 -0300

    Changes by mathe

--- src/main/java/com/care/health/ubs/UbsHealthCareApplication.java
+++ src/main/java/com/care/health/ubs/UbsHealthCareApplication.java
@@ -1,5 +1,6 @@
 package com.care.health.ubs;
 
+import org.apache.commons.collections4.KeyValue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
--- src/main/java/com/care/health/ubs/controller/ViewsController.java
+++ src/main/java/com/care/health/ubs/controller/ViewsController.java
@@ -1,10 +1,14 @@
 package com.care.health.ubs.controller;
 
+import com.care.health.ubs.UbsHealthCareApplication;
 import com.care.health.ubs.model.service.MedicoService;
+import org.hibernate.id.GUIDGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.UUID;
 
 @Controller
 public class ViewsController {
@@ -12,15 +16,17 @@ public class ViewsController {
     @Autowired
     private MedicoService service;
 
-    @GetMapping(path = "/")
+    @GetMapping(path = "/start")
     public String telaInicial(Model model){
         model.addAttribute("medicos", service.findAll());
+//        UbsHealthCareApplication.loginTokens.add("teste");
         return "Pages/Home/telaInicial";
     }
 
     @GetMapping(path = "/login")
     public String login(Model model){
         model.addAttribute("medicos", service.findAll());
+
         return "Pages/Login/login";
     }
 
